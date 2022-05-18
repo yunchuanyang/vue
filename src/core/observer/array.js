f928934b7d9347daa264f9d5711a4b2201a5d069
yunchuan <yunchuan90@126.com>
2022-05-18 10:37:48 +0800

    Update array.js

--- src/core/observer/array.js
+++ src/core/observer/array.js
@@ -30,13 +30,14 @@ methodsToPatch.forEach(function (method) {
     let inserted
     switch (method) {
       case 'push':
-      case 'unshift':
+      case 'unshift'://如果是push和unshift方法，传入参数就是新增元素
         inserted = args
         break
-      case 'splice':
+      case 'splice'://如果是splice方法，传入参数列表中下标为2的就是新增元素
         inserted = args.slice(2)
         break
     }
+    //把新增元素变为可侦测的响应式数据
     if (inserted) ob.observeArray(inserted)
     // notify change
     ob.dep.notify()
